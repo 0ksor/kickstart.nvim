12b113492fa83604cbe8c5360e70fd8565a22753
0ksorn <oguzaktas04@hotmail.com>
2025-07-25 00:39:31 +0200

    added non recursive search with Telescope. Both as keymap (<leader>sF) and as greeter (F) Deleted orgmode (was not usefull)

--- init.lua
+++ init.lua
@@ -1,86 +1,3 @@
---[[
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========                                    .-----.          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ======== ========        /::::::::::|  |::::::::::\  \ no mouse \     ======== ========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
@@ -173,7 +90,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 
 vim.opt.scrolloff = 10
 
@@ -286,6 +203,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>sF', ':FindHere<CR>', { desc = 'Find files non-recursive' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -352,31 +270,6 @@ require('lazy').setup({
     opts = {},
   },
   {
-    'nvim-orgmode/orgmode',
-    ft = 'org',
-    config = function()
-      require('orgmode').setup {}
-    end,
-  },
-  {
-    'lukas-reineke/headlines.nvim',
-    ft = { 'markdown', 'org', 'norg', 'asciidoc', 'tex' },
-    config = function()
-      require('headlines').setup {
-        org = {
-          enable = true,
-          markdown = true,
-          bold = true,
-          italic = true,
-          underline = true,
-          dash = '•',
-          fold_headlines = true, -- Başlık altını otomatik gizle
-        },
-        markdown = { enable = false }, -- Sadece org kullanıyorsan markdown kısmını kapatabilirsin
-      }
-    end,
-  },
-  {
     'kawre/leetcode.nvim',
     event = 'VeryLazy',
     build = ':TSUpdate html',
@@ -431,6 +324,13 @@ require('lazy').setup({
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       local dashboard = require 'alpha.themes.dashboard'
+
+      vim.api.nvim_create_user_command('FindHere', function()
+        require('telescope.builtin').find_files {
+          find_command = { 'find', '.', '-maxdepth', '1', '-type', 'f' },
+        }
+      end, {})
+
       dashboard.section.header.val = {
         [[                               __                ]],
         [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -439,28 +339,30 @@ require('lazy').setup({
         [[\ \_\ \_\ \____\ \____/\ \___/  \_\_\ \_\ \____/ ]],
         [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/___/  ]],
       }
+
       dashboard.section.buttons.val = {
         dashboard.button('e', 'New File', ':ene <BAR> startinsert<CR>'),
         dashboard.button('f', 'Find File', ':Telescope find_files<CR>'),
+        dashboard.button('F', 'Find Here (Non-recursive)', ':FindHere<CR>'),
         dashboard.button('r', 'Recent Files', ':Telescope oldfiles<CR>'),
         dashboard.button('s', 'Search Text', ':Telescope live_grep<CR>'),
         dashboard.button('q', 'Quit', ':qa<CR>'),
         dashboard.button('c', 'Config', ':e $MYVIMRC<CR>'),
         dashboard.button('C', 'i3 Config', ':e ' .. vim.fn.expand '~/.config/i3/config' .. '<CR>'),
         dashboard.button('<C-C>', 'Composer Config', ':e ' .. vim.fn.expand '~/.config/picom/picom.conf' .. '<CR>'),
-        -- Separator Line
         {
           type = 'text',
           val = '──────────────────────────────',
           opts = { position = 'center', hl = 'Comment' },
         },
-        -- Label for the Section
         { type = 'text', val = 'Frequently Used Files', opts = { position = 'center', hl = 'Title' } },
         dashboard.button('t', ' Tockens', ':e ' .. vim.fn.expand '~/.tokens' .. '<CR>'),
       }
+
       require('alpha').setup(dashboard.config)
     end,
   },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`.
   --
@@ -630,6 +532,8 @@ require('lazy').setup({
             '__pycache__/',
             '%.pyc$',
             'requirements.txt',
+            'html/',
+            'latex',
           },
         },
         pickers = {
