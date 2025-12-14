2c5ff07263144778b826efb259bbad88456958ec
0ksorn <oguzaktas04@hotmail.com>
2025-12-14 23:31:26 +0100

    true <-> false toggler function added

--- init.lua
+++ init.lua
@@ -57,17 +57,6 @@ vim.keymap.set('n', '<leader>v', function()
 end, { desc = '[B]ack to previous file' })
 
 -- o upgraded
--- vim.keymap.set('n', 'o', function()
---   local line = vim.api.nvim_get_current_line()
---   local row = vim.api.nvim_win_get_cursor(0)[1]
---
---   if line:find '{' and not line:find '}' then
---     vim.api.nvim_win_set_cursor(0, { row, line:find '{' })
---   end
---
---   vim.api.nvim_feedkeys('o', 'n', false)
--- end, { noremap = true, silent = true })
-
 vim.keymap.set('n', 'o', function()
   local line = vim.api.nvim_get_current_line()
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -81,6 +70,38 @@ vim.keymap.set('n', 'o', function()
     vim.api.nvim_feedkeys('o', 'n', false)
   end
 end, { noremap = true, silent = true })
+
+-- true <-> false toggle
+local function toggle_true_false()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_get_current_line()
+
+  -- sütun bazlı kelime arama
+  local start_col, end_col = line:find('true', col + 1)
+  local start_col2, end_col2 = line:find('True', col + 1)
+  if start_col then
+    -- true ise false yap
+    line = line:sub(1, start_col - 1) .. 'false' .. line:sub(end_col + 1)
+  elseif start_col2 then
+    line = line:sub(1, start_col2 - 1) .. 'False' .. line:sub(end_col2 + 1)
+  else
+    start_col, end_col = line:find('false', col + 1)
+    start_col2, end_col2 = line:find('False', col + 1)
+    if start_col then
+      -- false ise true yap
+      line = line:sub(1, start_col - 1) .. 'true' .. line:sub(end_col + 1)
+    elseif start_col2 then
+      line = line:sub(1, start_col2 - 1) .. 'True' .. line:sub(end_col2 + 1)
+    else
+      return -- ne true ne false bulundu
+    end
+  end
+
+  vim.api.nvim_set_current_line(line)
+end
+
+-- ss tuşunu normal modda bağla
+vim.keymap.set('n', 'ss', toggle_true_false, { noremap = true, silent = true })
 
 -- Enable break indent
 vim.opt.breakindent = true
